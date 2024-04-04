bb8e9101534ed4998f080d2b7a00e1204d712097
gildeanM <gildean.m098@gmail.com>
2024-04-04 10:41:46 -0300

    integration with zod and more routes

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,21 @@
 import fastify from 'fastify'
-import { z } from 'zod'
-import { PrismaClient } from '@prisma/client'
+import { serializerCompiler, validatorCompiler} from 'fastify-type-provider-zod'
+import { createEvent } from './routes/create-event'
+import { registerForEvent } from './routes/register-for-event'
+import { getEvent } from './routes/get-event'
+import { getAttendeeBadge } from './routes/get-attendee-badge'
+
 
 const app = fastify()
 
-const prisma = new PrismaClient({
-    log : ['query']
-})
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
-app.post('/events',async (request,reply)=>{
+app.register(createEvent)
+app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
-
-    const createEventSchema = z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-    })
-
-    const data = createEventSchema.parse(request.body)
-
-    const event = await prisma.event.create({
-        data: {
-            title: data.title,
-            details: data.details,
-            maximumAttendees: data.maximumAttendees,
-            slug: new Date().toISOString(),
-        },
-    })
-
-    // console.log(request.body)
-    return reply.status(201).send( {eventId : event.id} )
-})
 
 app.listen({ port:3333 }).then(() => {
     console.log("Server running on localhost:3333")
